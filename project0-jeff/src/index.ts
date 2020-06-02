fe6e3df3482da0329eee9878e2fa8f7e1e093059
Jeffrey Breiner <jeffreybreiner99@gmail.com>
2020-06-02 17:04:21 -0400

    debugging

--- project0-jeff/src/index.ts
+++ project0-jeff/src/index.ts
@@ -9,13 +9,15 @@ import { PoolClient, QueryResult } from "pg";
 import { findUserByUsernamePassword } from "./repository/user-data-access";
 import { loginRouter } from "./routers/loginRouter";
 import { reimbursementRouter } from "./routers/reimbursementRouter";
+import { corsFilter } from "./middleware/corsFilter"
 
-const PORT : number = 6464;
+const PORT: number = 6464;
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(sessionMiddleware);
 app.use(loggingMiddleware);
+app.use(corsFilter);
 
 app.use('/login', loginRouter);
 app.use('/users', userRouter);
@@ -23,10 +25,10 @@ app.use('/reimbursements', reimbursementRouter);
 
 app.get('/views', (req: Request, res: Response) => {
   console.log(req.session); // try to log it
-  if(req.session && req.session.views) {
+  if (req.session && req.session.views) {
     req.session.views++;
     res.send(`Reached this endpoint ${req.session.views} times`);
-  } else if(req.session) {
+  } else if (req.session) {
     req.session.views = 1;
     res.send('Reached the views endpoint for the first time');
   } else {
@@ -38,14 +40,14 @@ app.listen(6464, () => {
 
   console.log("app has started, testing connection:");
   connectionPool.connect().then(
-    (client: PoolClient)=>{
+    (client: PoolClient) => {
       console.log('connected');
       client.query('SELECT * FROM p0users;').then(
-        (result : QueryResult) => {
+        (result: QueryResult) => {
           console.log(result.rows[0]);
         }
       )
-  }).catch((err)=>{
-    console.error(err.message);
-  })
+    }).catch((err) => {
+      console.error(err.message);
+    })
 });
